fd4ac1288a8d3873a9b6990762811df668afb632
Azhar.Khan <naweed.khan@ndma.gov.gy>
2026-08-03 19:25:02 +0000

    Add Workspace fixture to fix undefined workspace content on staging

--- ndma_customizations_doctypes/hooks.py
+++ ndma_customizations_doctypes/hooks.py
@@ -249,3 +249,8 @@ doctype_js = {
 # ------------
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
+
+# Fixtures
+# ------------------
+
+fixtures = ["Workspace"]
