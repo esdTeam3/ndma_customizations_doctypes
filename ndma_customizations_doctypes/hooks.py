af388d792c65df6c4c150307aec4c436237c6064
Azhar.Khan <naweed.khan@ndma.gov.gy>
2026-08-04 12:41:09 +0000

    chore(hooks): reorganize fixtures and doctype_js sections

--- ndma_customizations_doctypes/hooks.py
+++ ndma_customizations_doctypes/hooks.py
@@ -51,6 +51,26 @@ doctype_js = {
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
+# include js in doctype views
+doctype_js = {
+	"Time Off": "public/js/time_off.js",
+	"Contract Renewal": "public/js/contract_renewal.js",
+}
+# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
+# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
+
+# Fixtures
+# ------------------
+
+fixtures = ["Workspace"]
+
+# Svg Icons
+# ------------------
+# include app icons in desk
+# app_include_icons = "ndma_customizations_doctypes/public/icons.svg"
+
+
 # Svg Icons
 # ------------------
 # include app icons in desk
@@ -249,8 +269,3 @@ doctype_js = {
 # ------------
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
-
-# Fixtures
-# ------------------
-
-fixtures = ["Workspace"]
